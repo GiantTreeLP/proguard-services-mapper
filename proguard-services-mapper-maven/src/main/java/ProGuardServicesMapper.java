cb51360d3f7c4a5a1e68341c958e2bd19eaa8586
GiantTree <GiantTreeLP@users.noreply.github.com>
2021-12-22 18:42:14 +0100

    Switch over to Java and Maven to circumvent issues in building the Maven plugin ("inspired" by the way JetBrains does it with the Kotlin compiler plugin)

---
+++ proguard-services-mapper-maven/src/main/java/ProGuardServicesMapper.java
@@ -0,0 +1,28 @@
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+import java.io.File;
+
+import static de.gianttree.proguardservicesmapper.common.MappingKt.mapServices;
+
+
+@Mojo(
+        name = ProGuardServicesMapper.ID,
+        defaultPhase = LifecyclePhase.PACKAGE
+)
+public class ProGuardServicesMapper extends AbstractMojo {
+    static final String ID = "map-proguard";
+
+    @Parameter(name = "input", required = true)
+    private File input;
+
+    @Parameter(name = "mapping", required = true)
+    private File mapping;
+
+    @Override
+    public void execute() {
+        mapServices(this.mapping.getAbsolutePath(), this.input.getAbsolutePath());
+    }
+}
